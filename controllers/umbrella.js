306c5da3f4848061d75583a5cff9838121492430
SSrinivasaRaju <vasusarikonda@gmail.com>
2023-04-21 10:14:18 -0500

    created new pugs

--- controllers/umbrella.js
+++ controllers/umbrella.js
@@ -49,7 +49,7 @@ exports.umbrella_view_all_Page = async function(req, res) {
    };
 
 
-   // Handle Costume create on POST.
+   // Handle umbrella create on POST.
 exports.umbrella_create_post = async function(req, res) {
     console.log(req.body)
     let document = new umbrella();
@@ -69,3 +69,109 @@ exports.umbrella_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
 };
+
+
+// for a specific umbrella.
+exports.umbrella_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await umbrella.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+
+
+    // Handle umbrella update form on PUT.
+    exports.umbrella_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await umbrella.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.Umbrella_color)
+    toUpdate.Umbrella_color = req.body.Umbrella_color;
+    if(req.body.Umbrella_length) toUpdate.Umbrella_length = req.body.Umbrella_length;
+    if(req.body.Umbrella_cost) toUpdate.Umbrella_cost = req.body.Umbrella_cost;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
+
+    // Handle umbrella delete on DELETE.
+exports.umbrella_delete = async function(req, res) {
+console.log("delete " + req.params.id)
+try {
+result = await umbrella.findByIdAndDelete( req.params.id)
+console.log("Removed " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": Error deleting ${err}}`);
+}
+};
+
+// Handle building the view for creating a umbrella.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.umbrella_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('umbrellacreate', { title: 'umbrella Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    
+
+// Handle a show one view with id specified by query
+exports.umbrella_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await umbrella.findById( req.query.id)
+    res.render('umbrelladetail',
+    { title: 'umbrella Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+    // Handle building the view for updating a umbrella.
+// query provides the id
+exports.umbrella_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await umbrella.findById(req.query.id)
+    res.render('umbrellaupdate', { title: 'umbrella Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+
+    // Handle a delete one view with id from query
+exports.umbrella_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await umbrella.findById(req.query.id)
+    res.render('umbrelladelete', { title: 'umbrella Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/umbrella.js
+++ routes/umbrella.js
@@ -1,6 +1,16 @@
 var express = require('express');
 const umbrella_controlers= require('../controllers/umbrella');
 var router = express.Router();
-/* GET costumes */
+/* GET umbrellas */
 router.get('/', umbrella_controlers.umbrella_view_all_Page );
+/* GET detail umbrella page */
+router.get('/detail', umbrella_controlers.umbrella_view_one_Page);
+/* GET create umbrella page */
+router.get('/create', umbrella_controlers.umbrella_create_Page);
+/* GET create update page */
+router.get('/update', umbrella_controlers.umbrella_update_Page);
+
+/* GET delete costume page */
+router.get('/delete', umbrella_controlers.umbrella_delete_Page);
+
 module.exports = router;
